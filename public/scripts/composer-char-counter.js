da4857e29032a04988994a669010a0b99075197f
RiazK <RiazCKhan@gmail.com>
2021-09-15 18:36:12 -0400

    Debug: tweeter form counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,22 +1,30 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 
-$("#tweet-text").on("input", function (event) {
-  console.log(this.input)
-})
+  $("#tweet-text").on("input", function (event) {
+    const tweetLength = this.value.length
+    const maxTweet = 10
+  
+    let tweetForm = $(this).closest('form');
+    let tweetCounter = tweetForm.find('.tweet-counter')
+    tweetCounter.html(140 - tweetLength);
 
-$("#tweet-text").on("input", function (event) {
-  console.log($("#tweet-text").val().length)
-})
-
-
-
-
-
-
-
-
-
+    console.log($form, 'mentor')
+    console.log($counter, 'mentor')
+    console.log($(this).find(".tweet-counter"), 'rck');
+    
+    if (tweetLength < maxTweet) {
+      console.log(tweetLength)
+    } else {
+      console.log("-" + tweetLength)
+    }
+  })
+  
+  
+  
+  
+  // console.log($("counter", this))
+  // j query selectors using 'this'
 
 
 
