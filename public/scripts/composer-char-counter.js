a7ad21d4e0c8b8659cd126751b3034ebf9851613
RiazK <RiazCKhan@gmail.com>
2021-09-15 18:44:13 -0400

    Update: implement colour in progress

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,21 +3,21 @@ $(document).ready(function () {
 
   $("#tweet-text").on("input", function (event) {
     const tweetLength = this.value.length
-    const maxTweet = 10
+    // const maxTweet = 10
   
     let tweetForm = $(this).closest('form');
     let tweetCounter = tweetForm.find('.tweet-counter')
     tweetCounter.html(140 - tweetLength);
 
-    console.log($form, 'mentor')
-    console.log($counter, 'mentor')
-    console.log($(this).find(".tweet-counter"), 'rck');
+    // console.log(tweetForm, 'mentor pt1')
+    // console.log(tweetCounter, 'mentor pt2')
+    // console.log($(this).find(".tweet-counter"), 'rck');
     
-    if (tweetLength < maxTweet) {
-      console.log(tweetLength)
-    } else {
-      console.log("-" + tweetLength)
-    }
+    // if (tweetLength < maxTweet) {
+    //   console.log(tweetLength)
+    // } else {
+    //   console.log("-" + tweetLength)
+    // }
   })
   
   
