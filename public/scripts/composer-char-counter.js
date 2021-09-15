a277ab760a5035e0033b0f71cae123bcb6d6e1fb
RiazK <RiazCKhan@gmail.com>
2021-09-15 16:39:59 -0400

    In progress: character count selector

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,30 @@
-console.log('reloaded')
+$(document).ready(function() {
+
+
+$("#tweet-text").on("input", function (event) {
+  console.log(this.input)
+})
+
+$("#tweet-text").on("input", function (event) {
+  console.log($("#tweet-text").val().length)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
