0812bc9dd1b7d8a40cc0ff8b2fe83248c10fa9fa
RiazK <RiazCKhan@gmail.com>
2021-09-16 18:53:53 -0400

    Complete: tweet container icon hover behaviour

--- public/scripts/tweet-container-behaviour.js
+++ public/scripts/tweet-container-behaviour.js
@@ -1,20 +1,25 @@
 $(document).ready(function () {
+  $(".tweet-container").hover(function () {
+    $(this).css('box-shadow', '5px 5px 1px rgb(0 0 0 / 0.2')
+  }, function () {
+    $(this).css('box-shadow', '0px 0px 0px rgb(0 0 0 / 0')
+  });
 
-$(".tweet-container").hover(function() {
-  console.log('i see')
-  $(this).css('box-shadow', '5px 5px 1px rgb(0 0 0 / 0.2')
-}, function () {
-  $(this).css('box-shadow', '0px 0px 0px rgb(0 0 0 / 0')
-});
+  $(".fas.fa-flag").hover(function () {
+    $(this).css('color', '#069B1E')
+  }, function () {
+    $(this).css('color', '#585858')
+  });
 
+  $(".fas.fa-heart").hover(function () {
+    $(this).css('color', '#9B0606')
+  }, function () {
+    $(this).css('color', '#585858')
+  });
 
-
-
-});
-
-
-
-$( "li.fade" ).hover(function() {
-  $( this ).fadeOut( 100 );
-  $( this ).fadeIn( 500 );
+  $(".fas.fa-retweet").hover(function () {
+    $(this).css('color', '#DED600')
+  }, function () {
+    $(this).css('color', '#585858')
+  });
 });
