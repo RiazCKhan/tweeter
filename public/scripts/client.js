6518bb26570dc374db30ac6c423d6670486349c0
RiazK <RiazCKhan@gmail.com>
2021-09-17 17:34:05 -0400

    Test: renderTweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,22 +1,49 @@
-/*
- - Client-side JS logic goes here
- - jQuery is already loaded
- - Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
 $(document).ready(function () {
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+
+  const tweetData = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  const renderTweets = function (tweets) {
+ 
+    tweetData.forEach(tweet => {
+      const keys = Object.keys(tweet)
+      for (let key of keys) {
+        console.log(createTweetElement(key))
+      }
+    })
+
+
+    // tweetData.forEach(tweet => {
+    //   for (let key in tweet) {
+    //     console.log(key);
+    //     // console.log(createTweetElement(key))
+    //   }
+    // })
   }
+  
 
   const createTweetElement = function (tweet) {
     const $tweet = `
@@ -48,10 +75,9 @@ $(document).ready(function () {
     return $tweet
   };
 
-  const $tweet = createTweetElement(tweetData);
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#all-tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  // const $tweet = createTweetElement(tweetData);
+  // $('#all-tweet-container').append($tweet);
 
-
+  // Temp Func Call
+  renderTweets() 
 });
