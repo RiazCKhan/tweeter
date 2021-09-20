31f47cd71d2425012cf69c66c25f8451e8cfc5cb
RiazK <RiazCKhan@gmail.com>
2021-09-20 19:16:09 -0400

    Replace: dated css behaviour+ Remove: commented code

--- public/scripts/tweet-container-behaviour.js
+++ public/scripts/tweet-container-behaviour.js
@@ -1,31 +1,3 @@
-// $(document).ready(function () {
-  // $(".tweet-container").hover(function () {
-  //   alert($this)
-  //   $(this).css('box-shadow', '5px 5px 1px rgb(0 0 0 / 0.2')
-  // }, function () {
-  //   $(this).css('box-shadow', '0px 0px 0px rgb(0 0 0 / 0')
-  // });
-
-  // $(".fas.fa-flag").hover(function () {
-  //   alert('msg')
-  //   $(this).css('color', '#069B1E')
-  // }, function () {
-  //   $(this).css('color', '#585858')
-  // });
-
-  // $(".fas.fa-heart").hover(function () {
-  //   $(this).css('color', '#9B0606')
-  // }, function () {
-  //   $(this).css('color', '#585858')
-  // });
-
-  // $(".fas.fa-retweet").hover(function () {
-  //   $(this).css('color', '#DED600')
-  // }, function () {
-  //   $(this).css('color', '#585858')
-  // });
-// });
-
 $(document).on("mouseenter", ".tweet-container", function(){
   $(this).css('box-shadow', '5px 5px 1px rgb(0 0 0 / 0.2')
 });
