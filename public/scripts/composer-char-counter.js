d08dbccb987aca4b42317b27dfb47ab0c4cb4e84
RiazK <RiazCKhan@gmail.com>
2021-09-25 13:19:16 -0400

    Remove: whitespace + unused binding, and apply eslint

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,13 @@
 $(document).ready(function () {
-  $("#tweet-text").on("input", function (event) {
+  $("#tweet-text").on("input", function () {
     const tweetLength = this.value.length
-  
     let tweetForm = $(this).closest('form'); // move out of 'this', i.e., #tweet-text to search the DOM Tree using 'closest' to find 'form' DOM Node
-    let tweetCounter = tweetForm.find('.tweet-counter') // from the form DOM Node use 'find' to locate '.tweet-counter'
+    let tweetCounter = tweetForm.find('.tweet-counter'); // from the form DOM Node use 'find' to locate '.tweet-counter'
 
     if (tweetLength < 140) {
       tweetCounter.html(140 - tweetLength).css("color", "#585858") // .html to access and alter the value found and stored in 'tweetCounter'
     } else {
       tweetCounter.html(140 - tweetLength).css("color", "red")
-    }
-  })  
+    };
+  }); 
 });
